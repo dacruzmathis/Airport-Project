d8d6a0995394a5585202d85c86a4c11642afb218
dacruzmathis <mathisdaccruz@gmail.com>
2022-12-27 14:13:23 +0100

    add gui class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,26 +12,8 @@ object Main {
     val stoAir = Storing.storingAirports()
     val stoRun = Storing.storingRunways()
 
+    GUI.GUI.menu(stoAir, stoCountries, stoRun)
 
-    println(s"\nChoose an option: \n\t (1) Query \n\t (2) Reports \n\t (0) Exit")
-    val selection = scala.io.StdIn.readInt()
-
-    selection match {
-      case 0 => exit
-
-      case 1 =>
-        println(s"\n ************* Query ************* \n")
-        println(s"Enter a Country Name or Country Code:")
-        Quering.readCountry(stoAir, stoCountries, stoRun)
-
-      case 2 =>
-        println(s"\n ************* Reports ************* \n")
-        println(s"\nChoose an option: \n\t (1) 10 countries with highest/lowest number of airports \n\t (2) Type of runways per country \n\t (3) Top 10 most common runway latitude \n\t2")
-        Report.readOption(stoAir, stoCountries, stoRun)
-
-      case _ =>
-        println("Input error -> Select only a number between 0 and 2 !")
-    }
     main(args: Array[String])
   }
 }
